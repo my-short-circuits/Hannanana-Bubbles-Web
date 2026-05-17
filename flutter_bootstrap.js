dd7c1b340a7c9132c947ef06c76a037a9c57499a
my-short-circuits <my.short.circuits@proton.me>
2026-05-17 15:17:24 -0400

    Deploy removal of daily and events shortcuts

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "432469620" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1788555759" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
