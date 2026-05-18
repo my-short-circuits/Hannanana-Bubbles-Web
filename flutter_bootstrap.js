58594fd36ebf18f6f7c96bbd366f4550aa67727e
my-short-circuits <my.short.circuits@proton.me>
2026-05-18 16:15:49 -0400

    Deploy secure leaderboard web build

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2805918581" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "256963550" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
